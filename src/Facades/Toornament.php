8b071356704765d662d5313f881ae4fd3b380a21
Poppabear <servnx@gmail.com>
2025-04-14 13:20:49 -0400

    add StageService and other service methods to Toornament facade

--- src/Facades/Toornament.php
+++ src/Facades/Toornament.php
@@ -10,7 +10,18 @@ use Illuminate\Support\Facades\Facade;
  * @method static \ServNX\Toornament\Services\MatchService match()
  * @method static \ServNX\Toornament\Services\RegistrationService registration()
  * @method static \ServNX\Toornament\Services\DisciplineService discipline()
+ * @method static \ServNX\Toornament\Services\StageService stage()
+ * @method static \ServNX\Toornament\Services\MatchReportService matchReport()
+ * @method static \ServNX\Toornament\Services\MatchGameService matchGame()
+ * @method static \ServNX\Toornament\Services\GroupService group()
+ * @method static \ServNX\Toornament\Services\BracketService bracket()
+ * @method static \ServNX\Toornament\Services\SponsorService sponsor()
+ * @method static \ServNX\Toornament\Services\RoundService round()
+ * @method static \ServNX\Toornament\Services\RankingService ranking()
+ * @method static \ServNX\Toornament\Services\CustomFieldService customField()
+ *
  * @method static \ServNX\Toornament\Http\ToornamentClient getClient()
+ *
  */
 class Toornament extends Facade
 {
